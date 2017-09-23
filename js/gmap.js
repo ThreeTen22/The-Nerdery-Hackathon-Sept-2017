4fd9cae300ffb2e76b5ca8fb246d08746bab3045
Brendan White <eggrollcontrol@gmail.com>
2017-09-23 16:40:31 -0500

    moved js files out of index

---
+++ js/gmap.js
@@ -0,0 +1,56 @@
+var map, infoWindow, markerArray = [], markerCluster, curCategory, dispData;
+
+var pos={
+  lat: 41.8781,
+  lng: -87.6298  
+};
+
+var zoomLevel=10;
+
+
+//This is the initialize the map on load
+function initMap() {  
+    /*user HTML5 geolocation to get browser location if success calls
+    the function to get coords and display map, else calls function
+    that will display with chicago as marker*/
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(geoLocSucess, geoLocFail);
+    
+    }
+    //this is for old browsers if it does not support geolocation
+    else{
+          pos.lat = 41.8781;
+          pos.lng = -87.6298;
+          displayMap();
+    }
+}
+
+
+//when location is enabled in browser and user allowed it
+function geoLocSucess(position){
+          pos.lat = position.coords.latitude;
+          pos.lng = position.coords.longitude;
+          displayMap();      
+}
+
+//when location is not enabled in browser or enabled but user blocked it
+function geoLocFail(position){
+          pos.lat = 41.8781;
+          pos.lng = -87.6298;
+          displayMap();    
+}
+
+
+//function to display the map when pos object has lat and lng
+function displayMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+            center: pos,
+            zoom: zoomLevel,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+
+    var initMarker = new google.maps.Marker({
+      position: new google.maps.LatLng(pos.lat,pos.lng),
+      map: map,     
+    });   
+}
