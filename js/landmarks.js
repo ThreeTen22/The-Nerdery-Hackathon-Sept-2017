cdfa0689b36e5524dabf58d27fadfc04cea615c9
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-09-23 18:38:10 -0500

    added list items

--- js/landmarks.js
+++ js/landmarks.js
@@ -1,13 +1,17 @@
+var landmark = []
 const getEneryData = () => {
     $.ajax({
         //url: "https://data.cityofchicago.org/resource/tepd-j7h5.json",
         url: "https://data.cityofchicago.org/resource/fpx9-pjqk.json",
         type: "GET",
         data: {
-            "$$app_token": "dAZz61L6NCJnpgbtQ7OXpToRI"
+            "$$app_token": "dAZz61L6NCJnpgbtQ7OXpToRI",
+            //"$limit": 10
         }
     }).done((data) => {
-        console.log(data)
+        
+        landmark = data
+//        console.log(landmark)
     })
 }
 
