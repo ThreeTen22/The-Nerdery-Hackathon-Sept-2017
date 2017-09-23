ec5b1e542dceb1f4e6c2e9b69acbd00ad6ba3388
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-09-23 14:07:18 -0500

    added landmarks data

--- js/landmarks.js
+++ js/landmarks.js
@@ -1,6 +1,7 @@
 const getEneryData = () => {
     $.ajax({
-        url: "https://data.cityofchicago.org/resource/tepd-j7h5.json",
+        //url: "https://data.cityofchicago.org/resource/tepd-j7h5.json",
+        url: "https://data.cityofchicago.org/resource/fpx9-pjqk.json",
         type: "GET",
         data: {
             "$$app_token": "dAZz61L6NCJnpgbtQ7OXpToRI"
